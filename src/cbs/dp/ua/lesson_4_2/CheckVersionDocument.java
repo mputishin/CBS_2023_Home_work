5129b8b0054608d046b46a878c00846c2cf7b2f2
Michael <mbigbear@gmail.com>
2023-07-30 18:20:44 +0300

    Change lesson_4_2

--- src/cbs/dp/ua/lesson_4_2/CheckVersionDocument.java
+++ src/cbs/dp/ua/lesson_4_2/CheckVersionDocument.java
@@ -3,12 +3,15 @@ package cbs.dp.ua.lesson_4_2;
 public class CheckVersionDocument {
 
     public AbstractHandler checkDoc (String version) {
-        if (version.equals("xml")) {
-            return new XMLHandler();
-        } else if (version.equals("doc")) {
-            return new DOCHandler();
-        } else if (version.equals("txt")) {
-            return new TXTHandler();
-        } else return new NOTDocumet();
+        switch (version) {
+            case "xml":
+                return new XMLHandler();
+            case "doc":
+                return new DOCHandler();
+            case "txt":
+                return new TXTHandler();
+            default:
+                return new NOTDocumet();
+        }
     }
 }
